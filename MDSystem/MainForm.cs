e0cff5d0b377ef11039249bbefebc4a0a01ec7df
afissenko <regalex82@gmail.com>
2023-08-25 18:05:05 +0600

    TestRevert-V1: add testProperty_v2

--- MDSystem/MainForm.cs
+++ MDSystem/MainForm.cs
@@ -18,6 +18,9 @@ namespace MDSystem
     public partial class MainForm : Form
     {
         string _testProperty = "Test property";
+
+        string _testProperty_v2 = "Test property_v2";
+
         public MainForm()
         {
             InitializeComponent();
